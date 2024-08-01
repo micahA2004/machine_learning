0c4b797322a0816ed59293444439ec0dae881d9a
Micah Andrejczak <micahandrejczak@tamu.edu>
2024-08-01 16:04:59 -0500

    Implemented testing, mean normalization, and sklearn for splitting up data. Working model

--- Regression/Real Models/mult_linreg_vec_algorithm.py
+++ Regression/Real Models/mult_linreg_vec_algorithm.py
@@ -1,32 +1,14 @@
+##deal with feature normalization
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import time as t
+import statistics as stat
+from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
 
 start = t.time()
-
-
-df = pd.read_csv("C:\\Users\\Micah Andrejczak\\Desktop\machine_learning\\Datasets\\Student_Performance.csv")
-
-#dropping this from dataframe because I am not sure how to deal with yes or no at this moment, or how to weight it
-#probably could just convert these vals to 1 or 0
-df = df.drop(["Extracurricular Activities"], axis = 1)
-
-#input data is everything except for target
-input_data = df.drop(["Performance Index"], axis = 1)
-target_data = df["Performance Index"]
-
-X = input_data.to_numpy()
-Y = target_data.to_numpy()
-
-#getting dimensions of data matrix
-shape = X.shape
-
-n = shape[1]
-
-#initial guesses for parameters
-w = np.zeros(n)
-b = 0
 
 model = lambda w,X,b: np.dot(X,w) + b #calculates model with given parameters and matrix X
 
@@ -102,11 +84,60 @@ def gradient_descent(w, X, b, Y, learning_rate, iteration_num, interval):
     
     return w, b, c
 
+def mean_normalization(w, X):
+    shape = X.shape
+    if len(w) != shape[0]:
+        X = X.T
+      
+    new = []
+    for (i, vec) in enumerate(X):
+        mean = stat.mean(vec)
+        rang = max(vec) - min(vec)  
+        vec = (vec - mean) / (rang)
+        new.append(vec)
+        
+    new = np.array(new)
+    return new
+    
+
+###TAKING CARE OF DATA PORTION###
+df = pd.read_csv("C:\\Users\\Micah Andrejczak\\Desktop\machine_learning\\Datasets\\Student_Performance.csv")
+
+df['Extracurricular Activities'] = df['Extracurricular Activities'].map({'Yes': 1, 'No': 0})
+
+test_ratio = 0.20
+train_df, test_df = train_test_split(df, test_size= test_ratio, random_state=42)  # 60% training, 40% testing
+
+# tdf,cdf = train_test_split()
+input_data_train = train_df.drop(["Performance Index"], axis = 1)
+target_data_train = train_df["Performance Index"]
+input_data_test = test_df.drop(["Performance Index"], axis = 1)
+target_data_test = test_df["Performance Index"]
+
+X = input_data_train.to_numpy()
+Y = target_data_train.to_numpy()
+X_test = input_data_test.to_numpy()
+Y_test = target_data_test.to_numpy()
+
+#getting dimensions of data matrix
+shape = X.shape
+
+n = shape[1]
+
+#initial guesses for parameters
+w = np.zeros(n)
+
+b = 0
+
+norm_X_train = mean_normalization(w, X)
+norm_X_test = mean_normalization(w, X_test)
+X_test = norm_X_test.T
+X = norm_X_train.T
 #alpha was found to be the fastest learning rate that doesn't allow divergence through testing
-alpha = 0.00025
+alpha = 0.1
 #modify either of these, higher iterations results in lower cost
 iterations = 10000
-interval = 1000
+interval = 100
 
 w,b,c = gradient_descent(w, X, b, Y, alpha, iterations, interval)
 
@@ -119,8 +150,8 @@ def create_model_string(w, b):
 
 #Prints the model rep
 model_str = create_model_string(w, b)
-print(f"Final model: y = {model_str}")
-print(f"Final cost is : {c}.")
+print(f"Final model: y = {model_str}\n")
+print(f"Final cost is : {c}.\n")
 
 
 end = t.time()
@@ -128,15 +159,46 @@ end = t.time()
 t_sec = end - start
 
 #Trying to plot all of the feature graphs
-feature_names = input_data.columns
-for i in range(n):
-    plt.figure()
-    plt.scatter(X[:, i], Y, label='Actual Data', color='blue')
-    plt.plot(X[:, i], model(w, X, b), label='Model Prediction', color='red')
-    plt.xlabel(f"Feature {feature_names[i]}")
-    plt.ylabel("Performance Index")
-    plt.title(f"Feature {feature_names[i]} vs. Performance Index")
-    plt.legend()
-    plt.show()
+# feature_names = input_data.columns
+# for i in range(n):
+#     plt.figure()
+#     plt.scatter(X[:, i], Y, label='Actual Data', color='blue')
+#     plt.plot(X[:, i], model(w, X, b), label='Model Prediction', color='red')
+#     plt.xlabel(f"Feature {feature_names[i]}")
+#     plt.ylabel("Performance Index")
+#     plt.title(f"Feature {feature_names[i]} vs. Performance Index")
+#     plt.legend()
+#     plt.show()
     
-print(f"Time this script takes in seconds:\n {t_sec} seconds.")
+print(f"Time this script takes in seconds:\n {t_sec} seconds.\n")
+
+# Writing the execution time to a file
+
+
+#at this point we have our regression model with normalized data, and we have our X_test matrix and Y_test vector
+#Let's now use the model to calculate the predicted y value and calculate percentages to evaluate accuracy
+
+#we will just make this with scalars first
+#remember percent error should just be (found value - actual value) / (actual value)
+def percent_error(exact, approximate):
+    num = approximate - exact
+    err = abs(num) / (exact)
+    return err
+
+Y_predicted = model(w, X_test, b)
+Y_test = Y_test
+
+err = percent_error(Y_test,Y_predicted)
+overall_err = np.sum(err) / len(err)
+
+
+percent_err = overall_err * 100
+
+testing_string = f"Final percent error of model vs. testing data is {percent_err:.3f}%.\n\n"
+conclusion = f"This means that the percent accuracy of this model is {(100-percent_err):.3f}%. "
+
+print(testing_string + conclusion)
+
+
+with open("execution_time_w_num_Iterations.txt", "a") as file:
+    file.write(f"{t_sec}, {iterations}, {test_ratio}, {percent_err}\n")
